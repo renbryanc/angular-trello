5ecaff3736119acc8b3e7b82503ef69c55feabd3
Tyler Rhodes <tyler.s.rhodes@gmail.com>
2014-06-11 08:32:38 -0700

    Rename 'myApp' to 'angularTrello'

--- app/js/app.js
+++ app/js/app.js
@@ -2,12 +2,12 @@
 
 
 // Declare app level module which depends on filters, and services
-angular.module('myApp', [
+angular.module('angularTrello', [
   'ngRoute',
-  'myApp.filters',
-  'myApp.services',
-  'myApp.directives',
-  'myApp.controllers'
+  'angularTrello.filters',
+  'angularTrello.services',
+  'angularTrello.directives',
+  'angularTrello.controllers'
 ]).
 config(['$routeProvider', function($routeProvider) {
   $routeProvider.when('/view1', {templateUrl: 'partials/partial1.html', controller: 'MyCtrl1'});
