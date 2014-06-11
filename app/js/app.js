e7e5b5c7029adfa52b1de7103373e5812dda5b78
Tyler Rhodes <tyler.s.rhodes@gmail.com>
2014-06-11 08:35:19 -0700

    Remove extra partials

--- app/js/app.js
+++ app/js/app.js
@@ -1,6 +1,5 @@
 'use strict';
 
-
 // Declare app level module which depends on filters, and services
 angular.module('angularTrello', [
   'ngRoute',
@@ -10,7 +9,6 @@ angular.module('angularTrello', [
   'angularTrello.controllers'
 ]).
 config(['$routeProvider', function($routeProvider) {
-  $routeProvider.when('/view1', {templateUrl: 'partials/partial1.html', controller: 'MyCtrl1'});
-  $routeProvider.when('/view2', {templateUrl: 'partials/partial2.html', controller: 'MyCtrl2'});
-  $routeProvider.otherwise({redirectTo: '/view1'});
+  $routeProvider.when('/', {templateUrl: 'partials/main_board.html', controller: 'MainBoardCtrl'});
+  $routeProvider.otherwise({redirectTo: '/'});
 }]);
