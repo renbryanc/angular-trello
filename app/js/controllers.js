9e9cee8c759bdd1c81b0dfd1c3ace7c2dfaff2c3
Tyler Rhodes <tyler.s.rhodes@gmail.com>
2014-06-11 17:11:03 -0700

    Add *very* basic columns

--- app/js/controllers.js
+++ app/js/controllers.js
@@ -4,5 +4,12 @@
 
 angular.module('angularTrello.controllers', [])
   .controller('MainBoardCtrl', ['$scope', function($scope) {
+    $scope.columns = {};
 
+    $scope.addColumn = function(name) {
+      $scope.columns[name] = {};
+    };
+
+    $scope.addColumn("Column 1");
+    $scope.addColumn("Column 2");
   }]);
